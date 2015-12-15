fe7e9bfaafe8e4498a1acd7e45c4b1fc6560f760
Maksim Kubrak <max.kmb@gmail.com>
2015-12-15 10:01:40 +0300

    Update category.sql
    исправлено и добавлено по п.1

--- category.sql
+++ category.sql
@@ -1,15 +1,17 @@
 1.
-mysql> select * from category where left(name, 4) like '%авто%' and parent_category_id is NULL;
+select * from category where left(name, 4) like '%авто%' and parent_category_id is NULL;
 a.
-mysql> select * from category where left(name, 4) like 'авто' and parent_category_id is NULL;
+select * from category where left(name, 4) like 'авто' and parent_category_id is NULL;
 b.
-mysql> select a.id, a.name from category a  left join category b on a.id = b. parent_category_id where b.id is NOT NULL and left(a.name, 4) like 'авто';
+select a.id, a.name from category a  left join category b on a.id = b. parent_category_id where b.id is NOT NULL and left(a.name, 4) like 'авто';
+c.
+select a.id, a.name from category a  left join category b on a.id = b. parent_category_id where b.id is NOT NULL and left(a.name, 4) like 'авто' group by a.id;
 
 2.
-mysql> select a.id, a.name from category a join category b on a.id = b.parent_category_id group by b.parent_category_id having count(b.parent_category_id)<=3;
+select a.id, a.name from category a join category b on a.id = b.parent_category_id group by b.parent_category_id having count(b.parent_category_id)<=3;
 
 3.
-mysql> select a.id, a.name from category a  left join category b on a.id = b. parent_category_id where b.id is NULL;
+select a.id, a.name from category a  left join category b on a.id = b. parent_category_id where b.id is NULL;
  
 alter table category add index (name);
 alter table category add index (parent_category_id);
